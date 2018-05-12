2c0eb6b163405da2e5c8036eb0a15ce2b2aebb9f
doikimihiro <doikimihiro@gmail.com>
2018-05-12 23:16:31 +0900

    WPF

---
+++ WpfApp/WpfApplication/Commands/OpenMessageDialogCommand.cs
@@ -0,0 +1,31 @@
+﻿namespace WpfApplication.Commands
+{
+    using System;
+    using System.Windows;
+    using System.Windows.Input;
+
+    public class OpenMessageDialogCommand : ICommand
+    {
+        public event EventHandler CanExecuteChanged;
+        
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+        
+        public void Execute(object parameter)
+        {
+            var name = parameter as string;
+
+            var message = "こんにちは、" + name + "さん";
+
+            MessageBox.Show(message);
+        }
+        
+    }
+}
+//MVVMではViewModel
+//OpenMessageDialogCommandはICommandを継承しているから、buttonのCommandプロパティに入れられる
+//ICommandはInterface
+//Interfaceは継承される前提で、定められたメソッドをサブクラスで実装する
+//CanExecuteChangedとCanExecuteとExcuteを定義しなければ、ICommandを継承することはできない
---
+++ WpfApp/WpfApplication/ViewModels/MainViewModel.cs
@@ -0,0 +1,20 @@
+﻿namespace WpfApplication.ViewModels
+{
+    using WpfApplication.Commands;
+    using WpfApplication.Models;
+
+    public class MainViewModel
+    {
+        public MainViewModel()
+        {
+            this.OpenMessageDialogCommand = new OpenMessageDialogCommand();
+            this.UserModel = new UserModel();
+        }
+
+        public OpenMessageDialogCommand OpenMessageDialogCommand { get; set; }
+
+        public UserModel UserModel { get; set; }
+    }
+}
+//ViewはCommandの具体的な中身を知らなくて良い
+//そうすることでViewはロジックを気にせずUIに専念できる
